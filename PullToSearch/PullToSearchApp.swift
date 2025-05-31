be893e1a61519d3f0e2b490078fb48ca65e8e4eb
rahimn20 <rahimnadeem1122@gmail.com>
2025-06-01 03:00:07 +0500

    Initial commit

---
+++ PullToSearch/PullToSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToSearchApp.swift
+//  PullToSearch
+//
+//  Created by Coder on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PullToSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PullToSearchTests/PullToSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  PullToSearchTests.swift
+//  PullToSearchTests
+//
+//  Created by Coder on 01/06/2025.
+//
+
+import Testing
+@testable import PullToSearch
+
+struct PullToSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
